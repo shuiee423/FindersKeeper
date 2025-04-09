2dc5a4c355d0cb58b24068a8215b5ca27548128d
shuiee423 <indino.samhenrie@gmail.com>
2025-04-09 23:33:06 +0800

    Importing Codes
    Log In page

---
+++ FKapp/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import Login from './Login';
+import SignUp from './SignUp';
+import { BrowserRouter as Router, Route, Routes, Link, HashRouter } from 'react-router-dom';
+
+
+function App() {
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/Signup" element={<SignUp />} />
+      </Routes>
+    </HashRouter>
+  );
+}
+
+export default App;
